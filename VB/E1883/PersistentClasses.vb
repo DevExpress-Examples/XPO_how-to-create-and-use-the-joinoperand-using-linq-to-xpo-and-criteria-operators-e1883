f7ce1cd5fbe513c541e9503304c97210f48f15ee
DevExpressExampleBot <fakeEmail>
2023-09-11 17:44:39 +0400

    autogenerate VB

--- VB/E1883/PersistentClasses.vb
+++ VB/E1883/PersistentClasses.vb
@@ -1,32 +1,36 @@
-﻿Imports DevExpress.Xpo
+Imports DevExpress.Xpo
+
 Namespace DXSample
-	Public Class Person
-		Inherits XPObject
 
-		Public Sub New(ByVal session As Session)
-			MyBase.New(session)
-		End Sub
+    Public Class Person
+        Inherits XPObject
 
-'INSTANT VB NOTE: The field name was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private name_Conflict As String
-		Public Property Name() As String
-			Get
-				Return name_Conflict
-			End Get
-			Set(ByVal value As String)
-				SetPropertyValue(Of String)("Name", name_Conflict, value)
-			End Set
-		End Property
+        Public Sub New(ByVal session As Session)
+            MyBase.New(session)
+        End Sub
 
-'INSTANT VB NOTE: The field age was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private age_Conflict As Integer
-		Public Property Age() As Integer
-			Get
-				Return age_Conflict
-			End Get
-			Set(ByVal value As Integer)
-				SetPropertyValue(Of Integer)("Age", age_Conflict, value)
-			End Set
-		End Property
-	End Class
+        Private nameField As String
+
+        Public Property Name As String
+            Get
+                Return nameField
+            End Get
+
+            Set(ByVal value As String)
+                SetPropertyValue(Of String)("Name", nameField, value)
+            End Set
+        End Property
+
+        Private ageField As Integer
+
+        Public Property Age As Integer
+            Get
+                Return ageField
+            End Get
+
+            Set(ByVal value As Integer)
+                SetPropertyValue(Of Integer)("Age", ageField, value)
+            End Set
+        End Property
+    End Class
 End Namespace
